41f6b8ac9a1fa9d56510aabde08991ae1b556a36
leejalen <leejalen@foxmail.com>
2020-09-13 22:31:06 +0800

    测试克隆项目中改变内容并上传到库中，与原项目上传到库中，比较是否冲突

--- HelloGit/src/main/java/testPack01/GitClass01.java
+++ HelloGit/src/main/java/testPack01/GitClass01.java
@@ -12,5 +12,6 @@ public class GitClass01 {
         System.out.println("你他妈有毛病");
         System.out.println("主干和分支添加同一个内容");
         System.out.println("测试分支合并后的结果");
+        System.out.println("测试克隆项目中改变内容并上传到库中，与原项目上传到库中，比较是否冲突");
     }
 }
