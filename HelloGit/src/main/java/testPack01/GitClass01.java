a1aa5b70a4ceeb31c40487986855f6e331e9fea0
leejalen <leejalen@foxmail.com>
2020-09-13 12:02:28 +0800

    更新GitClass01类

--- HelloGit/src/main/java/testPack01/GitClass01.java
+++ HelloGit/src/main/java/testPack01/GitClass01.java
@@ -9,5 +9,6 @@ public class GitClass01 {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("添加分支");
+        System.out.println("你他妈有毛病");
     }
 }
